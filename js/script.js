c7113ec35e8455df5bf107954a323ce0f11f3460
Will D <pleplu01@gmail.com>
2023-02-23 21:01:07 -0500

    Added a for loop
    Added a for loop that lists objects names and energy type from the array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 exoticWeapons = [
-    {name: "sunshot", energy: "solar", ammoType: "kinetic"},
-    {name: "riskrunner", energy: "arc", ammoType: "kinetic"},
-    {name: "fighting lion", energy: "void", ammoType: "special"}
+    {name: "Sunshot", weaponType: "Hand Cannon", energy: "Solar", ammoType: "Kinetic"},
+    {name: "Riskrunner", weaponType: "SMG", energy: "Arc", ammoType: "Kinetic"},
+    {name: "Fighting Lion", weaponType: "Grenade Launcher", energy: "Void", ammoType: "Special"}
 ];
+// An array of different exotic weapons
+
+for (let i = 0; i < exoticWeapons.length; i++) {
+    document.write(exoticWeapons[i].name + " (Energy: " + exoticWeapons[i].energy + ")");
+    if (exoticWeapons[i].energy === "Solar"){
+        document.write(" - this is a solar weapon. ");
+    }else if (exoticWeapons[i].energy === "Arc"){
+        document.write(" - this is an arc weapon. ");
+    }else {
+        document.write(" - this is a void weapon. ");
+    }
+  }
+// Lists objects names and energy types from the array 
+
+

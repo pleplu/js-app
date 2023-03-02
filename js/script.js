a2ccecc0c65d2f3634095a0e4e425635235e62c3
Will D <pleplu01@gmail.com>
2023-03-02 13:25:49 -0500

    Updated functions to align with FP and OOP standards
    Wrapped array into an IIFE and updated function to  better interact with said IIFE

--- js/script.js
+++ js/script.js
@@ -1,20 +1,80 @@
-exoticWeapons = [
-    {name: "Sunshot", weaponType: "Hand Cannon", energy: "Solar", ammoType: "Kinetic"},
-    {name: "Riskrunner", weaponType: "SMG", energy: "Arc", ammoType: "Kinetic"},
-    {name: "Fighting Lion", weaponType: "Grenade Launcher", energy: "Void", ammoType: "Special"}
-];
-// An array of different exotic weapons
+// An array of different exotic weapons wrapped inside of an IIFE :
 
-for (let i = 0; i < exoticWeapons.length; i++) {
-    document.write(exoticWeapons[i].name + " (Energy: " + exoticWeapons[i].energy + ")");
-    if (exoticWeapons[i].energy === "Solar"){
-        document.write(" - this is a solar weapon. " + "<br>");
-    }else if (exoticWeapons[i].energy === "Arc"){
-        document.write(" - this is an arc weapon. " + "<br>");
-    }else {
-        document.write(" - this is a void weapon. " + "<br>");
+let exoticWeaponsRepository = (function () {
+    let exoticWeapons = [
+        {name: "Sunshot", weaponType: "Hand Cannon", energy: "Solar", ammoType: "Primary"},
+        {name: "Riskrunner", weaponType: "SMG", energy: "Arc", ammoType: "Primary"},
+        {name: "Fighting Lion", weaponType: "Grenade Launcher", energy: "Void", ammoType: "Special"}
+    ];
+
+    function add(weapon) {
+        exoticWeapons.push(weapon);
     }
-  }
-// Lists objects names and energy types from the array 
 
+    function getAll() {
+        return exoticWeapons;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+// ==========================================================================================================
+
+// List object's names and characteristics using a "forEach" loop and a function both inside and otuside said loop as well as an arrow function:
+
+exoticWeaponsRepository.getAll().forEach(function(exoticWeapons) {
+    document.write("<p>" + exoticWeapons.name + " : " + exoticWeapons.weaponType);
+    if (exoticWeapons.energy === "Solar"){
+        document.write(" - this is a solar weapon. " + "</p>");
+    }else if (exoticWeapons.energy === "Arc"){
+        document.write(" - this is an arc weapon. " + "</p>");
+    }else if (exoticWeapons.energy === "Void"){
+        document.write(" - this is a void weapon. " + "</p>");
+    }else{
+        document.write(" - this weapon alternates between all three energy types. " + "</p>");
+    }
+});
+
+// exoticWeaponsRepository.getAll().forEach( exoticWeapons => document.write("<p>" + exoticWeapons.name + "</p>"));
+
+
+// function listNames(exoticWeapons) {
+//     document.write("<p>" + exoticWeapons.name + "</p>");
+// }
+ 
+// exoticWeaponsRepository.getAll().forEach(listNames);
+
+// ==========================================================================================================
+
+// Lists object's names and characteristics from the array using a "for" loop:
+
+// for (let i = 0; i < exoticWeaponsRepository.getAll().length; i++) {
+//     document.write("<p>" + exoticWeaponsRepository.getAll()[i].name + " : " + exoticWeaponsRepository.getAll()[i].weaponType);
+//     if (exoticWeaponsRepository.getAll()[i].energy === "Solar"){
+//         document.write(" - this is a solar weapon. " + "</p>");
+//     }else if (exoticWeaponsRepository.getAll()[i].energy === "Arc"){
+//         document.write(" - this is an arc weapon. " + "</p>");
+//     }else if (exoticWeaponsRepository.getAll()[i].energy === "Void"){
+//         document.write(" - this is a void weapon. " + "</p>");
+//     }else{
+//         document.write(" - this weapon alternates between all three energy types. " + "</p>");
+//     }
+//   }
+
+// ==========================================================================================================
+
+// Lists object names from array using a function and a "for" loop:
+
+// function printArrayDetails(list){
+//     for (let i = 0; i < list.length; i++){
+//         document.write("<p>" + list[i].name + "</p>")
+//     }
+// }
+
+// printArrayDetails(exoticWeaponsRepository.getAll());
+
+// ==========================================================================================================
 
